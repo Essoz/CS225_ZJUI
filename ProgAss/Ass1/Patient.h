76aa2f71b4e120acea9d8ec499d64c37bfb20636
SparkleStar <tianyuz.19@intl.zju.edu.cn>
2021-04-05 21:40:32 +0800

    Update

--- ProgAss/Ass1/Patient.h
+++ ProgAss/Ass1/Patient.h
@@ -3,9 +3,9 @@
 
 /*
  *  File: Patient.h
- *  
+ *
  *  Writter: Guo Moyang
- * 
+ *
  *  Description:
     This header file contains the class of "Patient", which has all the information:
     identification,risk,age,ddl,priority,withdraw,
@@ -13,15 +13,15 @@
     and appointment : date, time, location
     All above have their get and set functions
     for Appointment, There are extra functions : settime gettime; setdate getdate
-    for quick search and change of time & date; 
- *  
+    for quick search and change of time & date;
+ *
  */
 
 enum Risk {
-    no, low, medium, high
+    high, medium, low, no
 };
-enum Profession{
-    I,II,III,IV,V,VI,VII,VIII
+enum Profession {
+    I, II, III, IV, V, VI, VII, VIII
 };
 enum Age {
     children, adolescent, youngadult, adult, senior, elderly, old
@@ -40,8 +40,8 @@ typedef struct
 typedef struct
 {
     char location[10];
-    int date;       // The registeration date /*the format is YearMonthDate, Eg.20001204*/
-    int time;       // The precise registeration time /*the format is HourMinSec, Eg.184059*/
+    int year;       // The appointment year
+    int date;       // The appointment date, from 1-365
 } Appointment;
 
 class Patient
@@ -52,38 +52,63 @@ private:
     Profession profession;
     Age age;
     Information* information;
-    int date;// The registeration date /*the format is YearMonthDate, Eg.20001204*/
-    int time;// The precise registeration time /*the format is HourMinSec, Eg.184059*/
-    //char name[10];
-    //char email[10];
-    //int  phone[10];
-    bool withdraw;  // If the patient has withdrew before
+    int year;       // The registeration year
+    int date;        // The date for registeration, the range is 1-365
+    int withdraw;   // If the patient has withdrew before
+    // 0 for never withdraw, 1 for withdraw, 2 for withdraw and re-regiseter
     int letter_ddl; // The deadline of the priority letter, should be -1 if no letter is handed in
     int priority;   // The priority of treatment
     Appointment* appointment;
-    //char location[10];
-    //int date;       // The appointment date /*the format is YearMonthDate, Eg.20001204*/
-    //int time;       // The appointment time /*the format is HourMinSec, Eg.184059*/
+
 public:
-    Patient(int id,Profession professionLevel, Risk risk, Age a, Information *info, Appointment *appo,int date,int time, int ddl);      // Constructor for new patient
-    //~Patient();
+    Patient(int id, Risk risk, Profession professionLevel, Age a, Information* info, int year, int date, int withdraw, int ddl);      // Constructor for new patient
 
     /*set functions*/
     void setid(int id) {
         identificaiton = id;
     };
-    void setpro(Profession professionLevel) {
-        profession=professionLevel;
-    };
     void setrisk(Risk R) {
         risk = R;
+        // The medium risk has one month extension:
+        if (R == medium)
+        {
+            // Add the extension and increase the risk status:
+            risk = low;
+            date = date + 30;
+            // Go to the next year
+            if (date >= 365)
+            {
+                date = date % 365;
+                year = year + 1;
+            }
+        }
+    };
+    void setpro(Profession professionLevel) {
+        profession = professionLevel;
     };
     void setage(Age a) {
         age = a;
     }
-    void setwithdraw(bool W)
+    void setyear(int Y) {
+        year = T;
+    }
+    void setdate(int D) {
+        date = D;
+    }
+    void setwithdraw(int W)
     {
         withdraw = W;
+        // If the patient has re-registered again, add an extension of two weeks:
+        if (2 == W)
+        {
+            date = date + 14;
+            // Go to the next year
+            if (date >= 365)
+            {
+                date = date % 365;
+                year = year + 1;
+            }
+        }
     }
     void setddl(int ddl)
     {
@@ -93,44 +118,45 @@ public:
     {
         priority = prio;
     }
-    void setinfo(Information *info) {
+    void setinfo(Information* info) {
         information->phone = info->phone;
         information->phone = info->phone;
         for (int i = 0; i < 10; i++)
         {
-            information->name[i]=info->name[i];
-            information->email[i]=info->email[i];
+            information->name[i] = info->name[i];
+            information->email[i] = info->email[i];
         }
     }
-    void setappoint(Appointment *appo)
+    void setappoint(Appointment* appo)
     {
-       appointment->date = appo->date;
-       appointment->time = appo->time;
+        appointment->year = appo->year;
+        appointment->date = appo->date;
         for (int i = 0; i < 10; i++)
         {
-            appointment->location[i]=appo->location[i];
+            appointment->location[i] = appo->location[i];
         }
     }
-    void settime(int T){
-        appointment->time=T;
-    }
-    void setdate(int D){
-        appointment->date=D;
-    }
+
     /*get functions*/
     int getid() {
         return identificaiton;
     };
-    Profession getpro() {
-        return profession;
-    };
     Risk getrisk() {
         return risk;
+    };
+    Profession getpro() {
+        return profession;
     };
     Age getage() {
         return age;
     }
-    bool getwithdraw()
+    int getyear() {
+        return year;
+    }
+    int getdate() {
+        return date;
+    }
+    int getwithdraw()
     {
         return withdraw;
     }
@@ -142,50 +168,49 @@ public:
     {
         return priority;
     }
-
     Information* getinfo() {
         return information;
     }
-
     Appointment* getappoint()
     {
         return appointment;
     }
-    int getdate() {
-        return date;
-    }
-    int gettime() {
-        return time;
-    }
-    char* getlocation(){
+    char* getlocation() {
         return appointment->location;
     }
     // Other helper functions:
     int calculate_prio();     // Calculate the priority
 };
 
-Patient::Patient(int id,Profession professionLevel, Risk risk, Age a, Information *info, Appointment *appo,int date,int time, int ddl)
+Patient::Patient(int id, Risk risk, Profession professionLevel, Age a, Information* info, int year, int date, int withdraw, int ddl)
 {
     setid(id);
-    setpro(professionLevel);
-    setdate(date);
-    settime(time);
     setrisk(risk);
+    setpro(professionLevel);
     setage(a);
     setinfo(info);
-    //setdate(date);
-    //settime(time);
-    setwithdraw(false); // The new patient has no withdraw
+    setyear(year);
+    setdate(date);
+    setwithdraw(withdraw);
     setddl(ddl);
     // The priority needs to be calculated first:
     int prio = calculate_prio();
     setpriority(prio);
-    setappoint(appo);   // No appoinment yet
+    setappoint(NULL);   // No appoinment yet
 };
 
+// The priotiry is represented by a int number, different fields occupy different places (letter_ddl is not considered):
 int Patient::calculate_prio()
 {
     int prio = 0;
+    // Start from the registration time, they occupy the last 5 digits:
+    prio = prio + date + 1000 * (year - 2000);
+    // Then for age group, it stands for the least 6 digit:
+    prio = prio + 100000 * age;
+    // Then for profession, 7th digit:
+    prio = prio + 1000000 * profession;
+    // Finally the risk status, the 8th digit:
+    prio = prio + 10000000 * risk;
     return prio;
 }
 
