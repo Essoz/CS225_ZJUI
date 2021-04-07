6f10136a8e18f0fefbbeb1dafdb6fc72d8119327
JoshuaG0031 <Moyang.19@intl.zju.edu.cn>
2021-04-07 23:48:47 +0800

    G

--- ProgAss/Ass1/Local/Local_main.cpp
+++ ProgAss/Ass1/Local/Local_main.cpp
@@ -3,15 +3,44 @@
 #include "Local_hash.cpp"
 #include <stdio.h>
 #include <cstdlib>
+#include <string>
+#include <ctime>
 using namespace std;
 using std::cout;
 using std::cin;
 
 int main()
 {
-	// Starts with a queue
-	Queue* test = new Queue();
-	test->getio()->read_all("test1.csv", test);
+cout<<"Welcome to the local hospital registry."<<endl;
+cout<<"Please store the new patients in folder Local/new"<<endl;
+cout<<"Please store the patients that whose information need to update in folder Local/update"<<endl;
+Queue* test = new Queue();
+int Order=-1;
+string Filename="new/ExportCSV (0).csv";
+int Daycounter=0;
+string Filecounter1 =to_string(Daycounter);
+string Filecounter2 =to_string(Daycounter);
+while (Order!=0)
+{
+	cout<<"Today is day "<<Daycounter/2+1;cout<<" ";
+	if (Daycounter%2==0)
+	{
+		cout<<"morning"<<endl;
+	}
+	if (Daycounter%2!=0)
+	{
+		cout<<"afternoon"<<endl;
+	}
+	cout<<"Processing new patients:"<<endl;
+	Filecounter1=to_string(Daycounter++);
+	Filecounter2=Filecounter1;
+	Filename=Filename.replace(15,1,Filecounter1);
+	cout<<Filename<<endl;
+	test->getio()->read_all(Filename, test);
 	test->setl_queue(test->report(test->getl_queue()));
-	return 0;
+	cout<<"The file sent to the center has been packed into file named submit.csv";
+	cout<<"If you want to finish the Local registry program, enter 1,else enter other number"<<endl;
+	cin>>Order;
+}
+return 0;
 }
