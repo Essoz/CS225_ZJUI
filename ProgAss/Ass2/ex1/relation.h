9e51c54c8d182c09c905133a032528d38502a18a
essoz <yuxuanj0000@qq.com>
2021-05-16 19:10:55 +0800

    constructors for records appended

--- ProgAss/Ass2/ex1/relation.h
+++ ProgAss/Ass2/ex1/relation.h
@@ -22,10 +22,10 @@ class Block_Header{
         int64_t             getNum()                { return num_blks; }        // Number of blocks
         int64_t             getNumDeleted()         { return num_deleted; }     // ???
 
-        void        setNum(int64_t new_num)         { num_blks = new_num; }
-        void        setNumDeleted(int64_t new_num)  { num_deleted = new_num; }
+        void                setNum(int64_t new_num)         { num_blks = new_num; }
+        void                setNumDeleted(int64_t new_num)  { num_deleted = new_num; }
 
-        int64_t     find_block_id(int64_t min, int64_t max);
+        int64_t             find_block_id(int64_t min, int64_t max);
 
     private:
         int64_t             num_blks;
@@ -74,10 +74,10 @@ class Block{
         void            setmin_id(int64_t id)   { min_id = id; }
         void            setmax_id(int64_t id)   { max_id = id; }
 
-        content_t<T>*   Insert(content_t<T> record, Block_Header* header);       // Insert a record
+        content_t<T>*   Insert(content_t<T> record, Block_Header<T>* header);       // Insert a record
         content_t<T>    retrevial(int64_t id);  // Find a record with primary key id
         int64_t         FindIndex(int64_t id);  // Find the index of one record in the block
-        void            Delete(int64_t id, Block_Header* header);     // Delete a record with primart key id
+        void            Delete(int64_t id, Block_Header<T>* header);     // Delete a record with primart key id
         void            Merge(Block_Header<T>* header);
         void            Split(Block_Header<T>* header);
         void            Sort();
@@ -228,6 +228,9 @@ class Relation{
 /* <=== relational schema for PATIENT ===> */
 class person{
     public:
+        person(int64_t n_ID, int64_t n_address, int64_t n_med_id, 
+                int8_t n_profession, int8_t n_age, string n_name, 
+                string n_email, string n_dayborn);
         void        setID(int64_t id)               { ID = id; }
         void        setAddress(int64_t add)         { address = add; }
         void        setMedicalStatusID(int64_t id)  { med_id = id; }
@@ -256,10 +259,10 @@ class person{
         string      email;
         string      dayborn; // YY-MM-DD
 };
-
 /* <=== relational schema for MEDICAL STATUS ===> */
 class medical_status{
     public:
+        medical_status(int64_t n_ID, int8_t n_status);
         void        setID(int64_t id)           { ID = id; }
         void        setStatus(int8_t s)         { status = s; }
         int64_t     getID()                     { return ID; }
@@ -273,6 +276,9 @@ class medical_status{
 /* <=== relational schema for REGISTRATION ===> */
 class registration{
     public:
+        registration(int64_t n_ID, int64_t n_person_id, int64_t n_treatment_id,
+                    string n_deadline, string n_date, int8_t n_withdrawn, 
+                    bool n_assigned, int64_t n_assigned_loc, string n_assigned_date);
         void        setID(int64_t id)               { ID = id; }
         void        setPersonID(int64_t id)         { person_id = id; }
         void        setTreatmentID(int64_t id)      { treatment_id = id; }
@@ -309,6 +315,8 @@ class registration{
 /* <=== relational schema for TREATMENT ===> */
 class treatment{
     public:
+        treatment(int64_t n_ID, int64_t n_assigned_loc, int8_t n_treatment_type, 
+                string n_assigned_date, string n_end_date);
         void        setID(int64_t id)               { ID = id; }
         void        setAssignedLoc(int64_t loc)     { assigned_loc = loc; }
         void        setTreatType(int8_t type)       { treatment_type = type; }
@@ -328,5 +336,51 @@ class treatment{
         string      assigned_date;
         string      end_date;
 };
+
+person::person(int64_t n_ID, int64_t n_address, int64_t n_med_id, 
+                int8_t n_profession, int8_t n_age, string n_name, 
+                string n_email, string n_dayborn)
+                    : ID(n_ID)
+                    , address(n_address)
+                    , med_id(n_med_id)
+                    , profession(n_profession)
+                    , age(n_age)
+                    , name(n_name)
+                    , email(n_email)
+                    , dayborn(n_dayborn)
+{    
+}
+
+medical_status::medical_status(int64_t n_ID, int8_t n_status)
+                    : ID(n_ID)
+                    , status(n_status)
+{    
+}
+
+registration::registration(int64_t n_ID, int64_t n_person_id, int64_t n_treatment_id,
+                    string n_deadline, string n_date, int8_t n_withdrawn, 
+                    bool n_assigned, int64_t n_assigned_loc, string n_assigned_date)
+                    : ID(n_ID)
+                    , person_id(n_person_id)
+                    , treatment_id(n_treatment_id)
+                    , deadline(n_deadline)
+                    , date(n_date)
+                    , withdrawn(n_withdrawn)
+                    , assigned(n_assigned)
+                    , assigned_loc(n_assigned_loc)
+                    , assigned_date(n_assigned_date)
+{
+}
+
+treatment::treatment(int64_t n_ID, int64_t n_assigned_loc, int8_t n_treatment_type, 
+                    string n_assigned_date, string n_end_date)
+                    : ID(n_ID)
+                    , assigned_loc(n_assigned_loc)
+                    , treatment_type(n_treatment_type)
+                    , assigned_date(n_assigned_date)
+                    , end_date(n_end_date)
+{
+}
+
 
 #endif /* DATABASE_H */
